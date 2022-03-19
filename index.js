c872feb3bc588240d0d281a4f0d21a5b2c0daadf
Alisson Amaral <aalissonamaral98@gmail.com>
2022-03-19 15:15:59 -0300

    add regex to get the specific link and it's name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function getFile(filePath) {
   const encoding = "utf-8";
   try {
     const text = await fs.promises.readFile(filePath, encoding);
-    console.log(chalk.green(text));
+    console.log(getLinks(text));
   } catch (error) {
     handleError(error);
   }
@@ -13,6 +13,17 @@ async function getFile(filePath) {
 
 function handleError(error){
   throw new Error(chalk.red(error.code, 'there is no file in this filepath'));
+}
+
+function getLinks(text){
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const result = [];
+  let temp;
+  
+  while((temp = (regex.exec(text))) !== null){
+    result.push({[temp[1]]: temp[2]});
+  }
+  return result;
 }
 
 //* Then method
