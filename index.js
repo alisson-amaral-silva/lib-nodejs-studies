25be2d9a4cb422963af475752ea52aed75944d9a
Alisson Amaral <aalissonamaral98@gmail.com>
2022-03-19 13:03:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+
+const paragraph = 'Function';
+
+function text(string) {
+  return string;
+}
+
+console.log(text(paragraph));
